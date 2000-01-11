a82b6098d4ee25dfa47bbaaf1a6c401981fc45ca
David García Garzón <voki@canvoki.net>
2000-01-11 00:00:00 +0000

    Import of snapshot Memoria 20000111.zip

--- build.h
+++ build.h
@@ -1,7 +1,7 @@
 // Build Number Generator v1.0
 // Copyright (c) 1998 Vokimon/KKEPerians UNLTD
-#define BuildNumber 0x0279
-#define BuildDate "19990914 09:30:49"
+#define BuildNumber 0x02e7
+#define BuildDate "20000111 14:37:22"
 // Don't modify anything in this file but the hexadecimal number above
 // If something gets wrong, delete this file, run the generator, and
 // modify the hex number acordantly with the last version you remember
