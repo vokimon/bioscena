803cf6425a1a67cf9e7b0db2397089430e091fe5
David García Garzón <voki@canvoki.net>
2002-01-06 23:22:09 +0000

    Doxydocs added

--- src/Agent.h
+++ src/Agent.h
@@ -2,7 +2,7 @@
 //
 //////////////////////////////////////////////////////////////////////
 
-#if !defined(__KKEP_AGENT_H_INCLUDED)
+#ifndef __KKEP_AGENT_H_INCLUDED
 #define __KKEP_AGENT_H_INCLUDED
 
 #include <string>
@@ -17,6 +17,49 @@ using namespace std;
 
 class CBiosistema;
 
+
+/**
+* Defines the interface for enviromental agents.
+* Enviromental agents allow the user to control the media 
+* evolution along the time by ensambling the different 
+* kinds of them.
+* 
+* <h3>Triggering</h3>
+* 
+* Each environmental agent has an action assigned.
+* You may <b>trigger</b> this action by applying the 
+* calling operator (empty parenthesys) to it.
+* 
+* <h3>Agents subordination</h3>
+* 
+* Agents are <b>subordinate</b> each other this means 
+* that whenever an agent is triggered, it may trigger
+* another agent. Subordination must form an loopless
+* directed such that the whole graf can be accessed from
+* a root node. 
+* The most commont graph following this is rules is a simple tree.
+* 
+* Not all the agents call its subordinators, and not in
+* the same way. That's a feature because it allows having
+* subordinators agents such Probabilitzador (Probability based triggering),
+* Temporizador (Cicle based triggering) and Iterador 
+* (multible triggerings for each trigger).
+*
+* <h3>Queries on agents<h3>
+* 
+* Agents may also <b>query</b> each other values. 
+* In order to avoid query loops, querying an agent
+* must not imply subquerying another one, the queried 
+* value must be calculated on the last triggering of 
+* the queried agent.
+* 
+* <h3>Agent factory, activation and pasivation</h3>
+* 
+* You may use this abstract class as a factory class
+* for agents. You may create an agent of a concrete
+* type providing the string or integer identifier
+* of the subclass.
+*/
 class CAgent 
 {
 // Tipus Propis
@@ -25,16 +68,63 @@ public:
 // Algunes amigues
 	friend ostream& operator<< (ostream& str, CAgent& ag);
 // Construccio/Destruccio
-public:
+protected:
 	CAgent();
+public:
 	virtual ~CAgent();
 // Redefinibles
 public:
+	/** 
+	* Triggers the agent action.
+	* Redefine it on your own agents.
+	*/
 	virtual void operator() (void)=0;
-	virtual void dump(CMissatger & msg);
-	virtual bool configura(string parametre, istream & nom, t_diccionariAgents & diccionari, CMissatger & errors);
+	/**
+	* Gives a list of pointers to the subordinated agents.
+	* The way you redefine this function is to call the super
+	* subordinated list and then append the subordinated agents
+	* that your subclass adds.
+	* @return A collection of subordinated agents
+	*/
 	virtual list<CAgent*> subordinats() {list<CAgent*> l; return l;};
+	/**
+	* Gives a list of pointer to the queried agents.
+	* The way you redefine this function is to call the super
+	* dependencies list and then append the queried agents
+	* that your subclass adds.
+	* @return A collection of queried agents
+	*/
 	virtual list<CAgent*> dependencies() {list<CAgent*> l; return l;};
+protected:
+	/**
+	* Takes a configuration parameter from a configuration stream.
+	* When you derive an Agent from another, you must redeclare
+	* this function in order to intercept the configuration for
+	* the added parameters and parameters that you want to
+	* change the way they are configured.
+	* 
+	* If the received parameter is one of the taken you must
+	* return true, if not, you must call super::configura 
+	* function to give the superclass the chance to get it,
+	* and return whatever it returns, true or false.
+	* @param parametre The parameter name.
+	* @param content An input stream where to read the 
+	* parameter content from.
+	* @param dictionary An agent dictionary you may use in 
+	* order to get agents from their name. Notice that
+	* dictionary names are the names used in the configuration 
+	* source may not be the real one used in the activated agent tree. 
+	* @return True if the parameter has been accepted and the content
+	* parsing has been correct.
+	*/
+	virtual bool configura(string parameter, istream & content, t_diccionariAgents & diccionari, CMissatger & errors);
+	/**
+	* Dumps the agent configuration and state on the given stream.
+	* Redefine it by calling super's dump and then dumping the new 
+	* parameters on the stream.
+	* @params stream The stream you may dump the parameters.
+	*/
+	virtual void dump(CMissatger & stream);
 // Operacions
 public:
 	string nom(){return m_nom;};
@@ -69,4 +159,4 @@ public:
 	static void ProvaClasse();
 };
 
-#endif // !defined(__KKEP_AGENT_H_INCLUDED)
+#endif//__KKEP_AGENT_H_INCLUDED
