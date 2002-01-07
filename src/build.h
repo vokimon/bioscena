a6eb0925258692d3e26a6516d9ab4dd401b9b8ec
David García Garzón <voki@canvoki.net>
2002-01-07 01:55:08 +0000

    More builds

--- src/build.h
+++ src/build.h
@@ -1,7 +1,7 @@
 // Build Number Generator v1.4
 // Copyright (c) 1998-2001 Vokimon/KKEPerians UNLTD
-#define BuildNumber 0x0d93
-#define BuildDate "20011211 01:05:58"
+#define BuildNumber 0x0da3
+#define BuildDate "20020107 01:21:43"
 // Don't modify anything in this file but the hexadecimal number above
 // If something gets wrong, delete this file, run the generator, and
 // modify the hex number acordantly with the last version you remember
