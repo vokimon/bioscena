f0ccebc1407907ce576d50db776f63427673d875
David García Garzón <voki@canvoki.net>
2002-03-16 11:47:21 +0000

    Added the ControlAleatori class to the factory methods The parameter tipus on Crea is passes a const reference instead a per value string

--- src/ControlOrganisme.h
+++ src/ControlOrganisme.h
@@ -15,6 +15,21 @@
 #include "Gen.h"
 #include "Cariotip.h"
 
+/**
+* Abstracts the control structure that issues biological 
+* instructions for an organism.
+* An organism control is an structure that is able to issue 
+* instruction codes for an organism to execute.
+* When the organism borns, the control structure is 
+* initialized from the content of a Cariotip.
+* During life, the organism control uses the fenotipical
+* information to decide the instructions that will be issued.
+* <p>
+* Being both, the fenotype and the issued instruction simple 
+* integers, they are decoupled from their semantic meaning 
+* and gives some freedom on the concrete control implementation
+* to use.
+*/
 class CControlOrganisme
 {
 // Tipus propis
@@ -25,18 +40,36 @@ public:
 	virtual ~CControlOrganisme() {}
 // Operacions
 public:
-	// Construeix l'estructura de control a partir d'un cariotip
-	virtual bool init(CCariotip& c)=0;
-	// Deixa l'estructura com abans de l'init
+	/** 
+	* Constructs the control structure from a cariotype
+	* @param cariotip The molding cariotype
+	* @returns Whether the init has been succesfull
+	*/
+	virtual bool init(const CCariotip & cariotip)=0;
+	/**
+	* Resets the state of the control like if an init
+	* was never called. Is needed to call init a second 
+	* time.
+	*/
 	virtual void clear()=0;
-	// Dona una instruccio generada pel sistema de control
-	virtual uint32 seguentInstruccio(uint32 * fenotip)=0;
-	// Representa al CMissatger l'estructura de control (Estat actual?)
-	virtual void dump(CMissatger & msg)=0;
+	/**
+	* Selects an action code to execute.
+	* @param the fenotype
+	* @returns the action code
+	*/
+	virtual uint32 seguentInstruccio(const uint32 * fenotip)=0;
+	/**
+	* Dumps its internal status on an output stream for debug purposes.
+	* @param out the output stream
+	*/
+	virtual void dump(CMissatger & out)=0;
 // Operacions no redefinibles
 public:
-	// Retorna la cadena representant el tipus de Control
-	string tipus() {return m_tipus;};
+	/**
+	* Gives the string representing the Control type
+	* @returns A string that identifies the concrete Control type
+	*/
+	const string & tipus() {return m_tipus;};
 // Atributs
 protected:
 	string m_tipus;
@@ -44,11 +77,12 @@ protected:
 public:
 	static uint32 Nombre(void);
 	static CControlOrganisme * Crea(uint32 n);
-	static CControlOrganisme * Crea(string tipus);
+	static CControlOrganisme * Crea(const string&  tipus);
 // Proves
 public:
-	// No es pot fer virtual pura pero caldria fer-la en totes les subclasses
+	//* No es pot fer virtual pura pero caldria fer-la en totes les subclasses
 	static void ProvaClasse();
 };
 
 #endif // !defined(__KKEP_CONTROLORGANISME_H_INCLUDED)
+
