05f220056f151bd14a014af061c5e41bd5865377
David García Garzón <voki@canvoki.net>
2002-03-22 01:28:03 +0000

    *** empty log message ***

--- Src/build.h
+++ Src/build.h
@@ -1,7 +1,7 @@
 // Build Number Generator v1.4
 // Copyright (c) 1998-2001 Vokimon/KKEPerians UNLTD
-#define BuildNumber 0x0db3
-#define BuildDate "20020316 12:33:31"
+#define BuildNumber 0x0db7
+#define BuildDate "20020321 05:27:56"
 // Don't modify anything in this file but the hexadecimal number above
 // If something gets wrong, delete this file, run the generator, and
 // modify the hex number acordantly with the last version you remember
