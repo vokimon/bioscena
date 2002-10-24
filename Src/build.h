97b7ffa803f3b499102606b4841dc96f9c47d0a3
David García Garzón <voki@canvoki.net>
2002-10-24 08:16:09 +0000

    + alcada -> height + amplada -> width + height and width getters

--- Src/build.h
+++ Src/build.h
@@ -1,7 +1,7 @@
 // Build Number Generator v1.4
 // Copyright (c) 1998-2001 Vokimon/KKEPerians UNLTD
-#define BuildNumber 0x0dc3
-#define BuildDate "20021023 01:32:52"
+#define BuildNumber 0x0dd7
+#define BuildDate "20021024 10:07:21"
 // Don't modify anything in this file but the hexadecimal number above
 // If something gets wrong, delete this file, run the generator, and
 // modify the hex number acordantly with the last version you remember
