04eaa0eb7730637bb5e2b615052789a9785201fe
David García Garzón <voki@canvoki.net>
2002-12-22 21:59:07 +0000

    Translation to english

--- src/Topologia.cpp
+++ src/Topologia.cpp
@@ -12,9 +12,9 @@ using namespace Bioscena;
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
-Topology::Topology(uint32 tamany)
+Topology::Topology(uint32 size)
 {
-	m_totalCasselles=tamany;
+	m_totalCasselles=size;
 }
 
 Topology::~Topology()
@@ -25,32 +25,32 @@ Topology::~Topology()
 // Redefinibles
 //////////////////////////////////////////////////////////////////////
 
-uint32 Topology::tamany() const
+uint32 Topology::size() const
 {
 	return m_totalCasselles;
 }
 
-bool Topology::esPosicioValida(t_position pos) const
+bool Topology::isValidPosition(t_position pos) const
 {
 	return (pos<m_totalCasselles)&&(pos>=0);
 }
 
-Topology::t_position Topology::displace (t_position origen, t_displacement movimentRelatiu) const
-// Retorna la posicio resultant de fer el displacement des de l'origen
+Topology::t_position Topology::displace (t_position origin, t_displacement movimentRelatiu) const
+// Retorna la posicio resultant de fer el displacement des de l'origin
 {
 	// TODO: Per defecte aillades o indeterministic?
-	return movimentRelatiu ? posicioAleatoria() : origen;
-//	return origen;
+	return movimentRelatiu ? randomPosition() : origin;
+//	return origin;
 }
 
-Topology::t_position Topology::displaceRandomly (t_position origen, uint32 radi) const
-// Retorna la posicio resultant de tants displacements aleatoris des de l'origen com indiqui el radi
+Topology::t_position Topology::displaceRandomly (t_position origin, uint32 radi) const
+// Retorna la posicio resultant de tants displacements aleatoris des de l'origin com indiqui el radi
 {
-	while (radi--) origen = displace (origen, rnd.get());
-	return origen;
+	while (radi--) origin = displace (origin, rnd.get());
+	return origin;
 }
 
-Topology::t_displacement Topology::invers (t_displacement desp) const
+Topology::t_displacement Topology::opositeDisplacement (t_displacement desp) const
 // Retorna un displacement invers al displacement
 {
 	// TODO: Per defecte aillades o indeterministic?
@@ -66,7 +66,7 @@ Topology::t_displacement Topology::nilDisplacement () const
 	return 0;
 }
 
-bool Topology::unio (t_position posOrigen, t_position posDesti, t_displacement & desp) const
+bool Topology::pathTowards (t_position posOrigen, t_position posDesti, t_displacement & desp) const
 // Retorna cert si es posible unir-les amb un sol displacement, a desp hi es
 // el displacement per unir-les o apropar-les
 {
@@ -74,13 +74,13 @@ bool Topology::unio (t_position posOrigen, t_position posDesti, t_displacement &
 	return false;
 }
 
-Topology::t_position Topology::posicioAleatoria() const
+Topology::t_position Topology::randomPosition() const
 {
 	CRandomStream rnd;
 	return rnd.get(0,m_totalCasselles-1);
 }
 
-uint32 Topology::distancia(t_position posOrigen, t_position posDesti) const
+uint32 Topology::distance(t_position posOrigen, t_position posDesti) const
 {
 	return 0;
 }
@@ -92,18 +92,18 @@ uint32 Topology::distancia(t_position posOrigen, t_position posDesti) const
 using namespace AnsiCodes;
 
 void Topology::ProvaClasse(void) {
-	// TODO: Proves Topologia::unio
+	// TODO: Proves Topologia::pathTowards
 	uint32 i;
 	int escala[]={0,6,3,4,12,14,15,7};
 	Topology topologia(400);
-	uint32 * celles = new uint32[topologia.tamany()];
-	for (i=topologia.tamany();i--;) celles[i]=0;
+	uint32 * celles = new uint32[topologia.size()];
+	for (i=topologia.size();i--;) celles[i]=0;
 	t_position cuc[7]={130,130,130,130,130,130,130};
 	while (cuc[0]!=8) {
 		for (i=7;i--;) celles[cuc[i]]=escala[6-i];
-		celles[topologia.posicioAleatoria()]=8;
+		celles[topologia.randomPosition()]=8;
 		out << gotoxy(1,1) << hex;
-		for (i=0; i<topologia.tamany(); i++)
+		for (i=0; i<topologia.size(); i++)
 			out << setw(1) << CColor(celles[i]) << "#";
 		out << dec << endl;
 		CRandomStream rnd;
@@ -111,7 +111,7 @@ void Topology::ProvaClasse(void) {
 		rnd>>direccio;
 		for (i=6;i--;) cuc[i+1]=cuc[i];
 		cuc[0]=topologia.displace(cuc[0],direccio);
-		if (cuc[0]==cuc[6]) cuc[0]=topologia.posicioAleatoria();
+		if (cuc[0]==cuc[6]) cuc[0]=topologia.randomPosition();
 		out<<blanc.fosc()<<"Celles"<<setw(5)<<setfill(' ');
 		for (i=7;i--;) out << cuc[i] << " - ";
 		out<<setw(0)<<endl;
