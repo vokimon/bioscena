eedb64228fc2e0e60265d1f15dc035a6de49fc24
David García Garzón <voki@canvoki.net>
2003-03-13 00:25:27 +0000

    Files moved to Utils and ported to modern code guidelines

---
+++ Src/Utils/Compatibility.hxx
@@ -0,0 +1,116 @@
+// Compatibilitat.h: interface for the Compatibilitat templates.
+//
+//////////////////////////////////////////////////////////////////////
+// TODO:
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(__KKEP_COMPATIBILITAT_H_INCLUDED)
+#define __KKEP_COMPATIBILITAT_H_INCLUDED
+
+//#include <time.h>
+//#include <stdlib.h>
+#include "RandomStream.h"
+
+namespace Bioscena {
+
+typedef uint32 t_clau;
+
+inline unsigned comptaUns (t_clau n)
+{
+	int res;
+	for (res=0; n; n>>=1) 
+		if (n&1) res++;
+	return res;
+};
+
+inline uint32 nibble(uint32 numero, uint32 valor) 
+{
+	return (valor >> (numero<<2)) & 0x0000000F;
+}
+
+/**
+* Compatibility keys are used to know how two numbers correspond each
+* other.
+* The probability for one key to match another is proportional to the 
+* binary coincidence and should be 
+* 
+*/
+
+class Key {
+	public:
+		bool isCompatibleWith(Key otherKey, t_tolerance tolerance) {
+			return compat<selectedAlgoritm>(m_key, otherKey.m_key, tolerance);
+		}
+		static bool areCompatibles(Key aKey, Key otherKey, t_tolerance tolerance) {
+			return compat<selectedAlgoritm>(aKey.m_key, otherKey.m_key, tolerance);
+		}
+	private:
+		typedef uint32 t_tolerance;
+
+	private:
+		enum {selectedAlgoritm=2};
+		uint3 m_key;
+		
+		template <unsigned algoritm=selectedAlgoritm>
+		static bool compat(t_clau k1, t_clau k2, uint32 tolerancia);
+
+		///////////////////////////////////////////////////////////////////////
+		/// Funcio de compatibilitat de claus no 1
+		///    Tolerancia al numero d'uns
+		///    Rendiment: un 'rand' i un ComptaUns
+		///    Funcio: ??
+		///    Comentari: ??
+		///////////////////////////////////////////////////////////////////////
+		template <>
+		static bool compat<1> (t_clau k1, t_clau k2, uint32 tolerancia) {
+		//	t_clau ruleta = (rand()>>10);
+			t_clau ruleta = rnd.get();
+			t_clau coincidencia = comptaUns( ~(k1 ^ k2));
+		//	return (ruleta>>tolerancia)<(coincidencia);
+			return ruleta>>(tolerancia&0x7) < (coincidencia<<((tolerancia>>3)&0xf));
+			}
+
+		//////////////////////////////////////////////////////////////////////
+		/// Funcio de compatibilitat no 2
+		///    Tolerancia a la posicio dels uns
+		///    Rendiment: dos 'rand'
+		///    Funcio: Massa restrictiva i poc color
+		///    Comentari: Per lo optima que es no esta gens malament.
+		//////////////////////////////////////////////////////////////////////
+		template <>
+		static bool compat<2> (t_clau k1, t_clau k2, t_clau tolerancia) {
+			t_clau ruleta = rnd.get();
+			return (ruleta&(k1^k2)&~tolerancia) == 0;
+			}
+
+		///////////////////////////////////////////////////////////////////////
+		/// Funcio de compatibilitat 3
+		///    Tolerancia a la quantitat dels uns
+		///    Rendiment: un 'ComptaUns' i dos 'rand'
+		///    Comentari: La coincidencia i la tolerancia no son simetriques
+		///////////////////////////////////////////////////////////////////////
+		template <>
+		static bool compat<3> (t_clau k1, t_clau k2, uint32 tolerancia) {
+			t_clau ruleta = rnd.get();
+			return comptaUns(ruleta&(k1^k2))<tolerancia;
+			}
+
+		///////////////////////////////////////////////////////////////////////
+		/// Funcio de compatibilitat no 4
+		///    Tolerancia a la posicio dels uns
+		///    Rendiment: un 'ComptaUns' i tres 'rand'
+		///    Comentari: Modifica la 2 per ser menys restrictiva a la zona 
+		///////////////////////////////////////////////////////////////////////
+		template <>
+		static bool compat<4> (t_clau k1, t_clau k2, t_clau tolerancia) {
+			t_clau ruleta = rnd.get();
+			t_clau ruleta2 = rnd.get(0,6);
+			return comptaUns(ruleta&(k1^k2)&~tolerancia) <= ruleta2;
+			}
+
+};
+
+
+}
+
+#endif // !defined(__KKEP_COMPATIBILITAT_H_INCLUDED)
---
+++ Src/Utils/Roulette.test.cxx
@@ -0,0 +1,177 @@
+// Roulette.test.cxx: regresion test for the Roulette class.
+//
+//////////////////////////////////////////////////////////////////////
+// TODO:
+// - Distribution tests
+//   - Distribution test assertion
+//   - Distribution test construction
+// - Performance tests
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestFixture.h>
+
+#include "Roulette.hxx"
+#include <time.h>
+
+using Bioscena::Roulette;
+
+namespace BioscenaTest {
+
+class RouletteConstructionTest : public CppUnit::TestCase {
+	typedef CppUnit::TestCase super;
+	CPPUNIT_TEST_SUITE( RouletteConstructionTest );
+	CPPUNIT_TEST( testQueryVoidReturnsZero );
+	CPPUNIT_TEST( testSetSectorFirstTime );
+	CPPUNIT_TEST( testResetSectorValue);
+	CPPUNIT_TEST( testDeleteSector);
+	CPPUNIT_TEST_SUITE_END();
+	Roulette * _roulette0;
+public:
+	void setUp()
+	{
+		_roulette0 = new Roulette;
+	}
+	void tearDown() {
+//		_roulette0->dump();
+		delete _roulette0;
+	}
+protected:
+	void testQueryVoidReturnsZero() {
+		CPPUNIT_ASSERT_EQUAL(uint32(0),_roulette0->sector(3));
+		CPPUNIT_ASSERT(_roulette0->checkInvariant());
+	}
+	void testSetSectorFirstTime() {
+		_roulette0->sector(3,5);
+		CPPUNIT_ASSERT_EQUAL(uint32(5),_roulette0->sector(3));
+		CPPUNIT_ASSERT(_roulette0->checkInvariant());
+	}
+	void testResetSectorValue() {
+		_roulette0->sector(3,5);
+		_roulette0->sector(3,4);
+		CPPUNIT_ASSERT_EQUAL(uint32(4),_roulette0->sector(3));
+		CPPUNIT_ASSERT(_roulette0->checkInvariant());
+	}
+	void testDeleteSector() {
+		_roulette0->sector(3,5);
+		_roulette0->sector(3,0);
+		CPPUNIT_ASSERT_EQUAL(uint32(0),_roulette0->sector(3));
+		CPPUNIT_ASSERT(_roulette0->checkInvariant());
+	}
+	void test() {
+		_roulette0->sector(3,5);
+		_roulette0->sector(3,0);
+		CPPUNIT_ASSERT_EQUAL(uint32(0),_roulette0->sector(3));
+		CPPUNIT_ASSERT(_roulette0->checkInvariant());
+	}
+};
+
+class RouletteOperationTest : public CppUnit::TestCase {
+	typedef CppUnit::TestCase super;
+	CPPUNIT_TEST_SUITE( RouletteOperationTest );
+	CPPUNIT_TEST( testSpinUniformWithMinimumForce );
+	CPPUNIT_TEST( testSpinUniformWithMaximumForce );
+	CPPUNIT_TEST( testSpinUniformWithMediumForce );
+	CPPUNIT_TEST( testSpinUniformWithExtraForce );
+	CPPUNIT_TEST_SUITE_END();
+	Roulette * _uniformRoulette;
+public:
+	void setUp()
+	{
+		_uniformRoulette = new Roulette;
+		_uniformRoulette->sector(10,1);
+		_uniformRoulette->sector(11,1);
+		_uniformRoulette->sector(12,1);
+		_uniformRoulette->sector(13,1);
+		_uniformRoulette->sector(14,1);
+		_uniformRoulette->sector(15,1);
+		_uniformRoulette->sector(16,1);
+		_uniformRoulette->sector(17,1);
+		_uniformRoulette->sector(18,1);
+		_uniformRoulette->sector(19,1);
+
+	}
+	void tearDown() {
+		delete _uniformRoulette;
+	}
+protected:
+	void testSpinUniformWithMinimumForce() {
+		CPPUNIT_ASSERT_EQUAL(uint32(10),_uniformRoulette->spin(0));
+	}
+	void testSpinUniformWithMaximumForce() {
+		CPPUNIT_ASSERT_EQUAL(uint32(19),_uniformRoulette->spin(9));
+	}
+	void testSpinUniformWithMediumForce() {
+		CPPUNIT_ASSERT_EQUAL(uint32(15),_uniformRoulette->spin(5));
+	}
+	void testSpinUniformWithExtraForce() {
+		try {
+			_uniformRoulette->spin(10);
+			CPPUNIT_FAIL("Extra spinned roulette, did not throw an exception");
+		}
+		catch (Bioscena::ErrAssertionFailed) {
+			//TODO: Check that the assertion is ok
+		}
+	}
+};
+
+class RouletteDistributionTest : public CppUnit::TestCase {
+	typedef CppUnit::TestCase super;
+	CPPUNIT_TEST_SUITE( RouletteDistributionTest );
+	CPPUNIT_TEST( testUniformSectors );
+	CPPUNIT_TEST_SUITE_END();
+	Roulette * _uniformRoulette;
+public:
+	void setUp()
+	{
+		_uniformRoulette = new Roulette;
+		_uniformRoulette->sector(10,1);
+		_uniformRoulette->sector(11,1);
+		_uniformRoulette->sector(12,1);
+		_uniformRoulette->sector(13,1);
+		_uniformRoulette->sector(14,1);
+		_uniformRoulette->sector(15,1);
+		_uniformRoulette->sector(16,1);
+		_uniformRoulette->sector(17,1);
+		_uniformRoulette->sector(18,1);
+		_uniformRoulette->sector(19,1);
+
+	}
+	void tearDown() {
+		delete _uniformRoulette;
+	}
+protected:
+	bool assertDistributionErrorAcceptable(double * ) {
+		return false;
+	}
+protected:
+	void testUniformSectors() {
+		const uint32 niterations = 0xfffff;
+		uint32 histograma[10] = {};
+		time_t t= time(NULL);
+		for (int i = niterations; i--;) {
+			uint32 value=(*_uniformRoulette);
+			CPPUNIT_ASSERT(value>=10 && value <20);
+			histograma[value-10]++;
+		}
+/*
+		std::cout << "Elapsed preallocated object: " << (time(NULL)-t) << std::endl;
+		for (int i = 10; i--;) {
+			std::cout << (double)histograma[i]/niterations << std::endl;;
+		}
+*/
+	}
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(RouletteConstructionTest);
+CPPUNIT_TEST_SUITE_REGISTRATION(RouletteOperationTest);
+CPPUNIT_TEST_SUITE_REGISTRATION(RouletteDistributionTest);
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( RouletteConstructionTest, "RouletteTests");
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( RouletteOperationTest, "RouletteTests");
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( RouletteDistributionTest, "RouletteTests");
+
+}
+/*
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( ExceptionTest,
+	CppUnitTest::coreSuiteName() );
+*/
+
+
