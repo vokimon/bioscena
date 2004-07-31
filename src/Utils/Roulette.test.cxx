ca1c5c783af0f05d510873309ed9bd0499cc7752
David García Garzón <voki@canvoki.net>
2004-07-31 11:23:37 +0000

    Proportional distribution

--- src/Utils/Roulette.test.cxx
+++ src/Utils/Roulette.test.cxx
@@ -79,39 +79,37 @@ class RouletteOperationTest : public CppUnit::TestCase {
 	CPPUNIT_TEST( testSpinUniformWithMediumForce );
 	CPPUNIT_TEST( testSpinUniformWithExtraForce );
 	CPPUNIT_TEST_SUITE_END();
-	Roulette * _uniformRoulette;
+	Roulette _uniformRoulette;
 public:
 	void setUp()
 	{
-		_uniformRoulette = new Roulette;
-		_uniformRoulette->sector(10,1);
-		_uniformRoulette->sector(11,1);
-		_uniformRoulette->sector(12,1);
-		_uniformRoulette->sector(13,1);
-		_uniformRoulette->sector(14,1);
-		_uniformRoulette->sector(15,1);
-		_uniformRoulette->sector(16,1);
-		_uniformRoulette->sector(17,1);
-		_uniformRoulette->sector(18,1);
-		_uniformRoulette->sector(19,1);
+		_uniformRoulette.sector(10,1);
+		_uniformRoulette.sector(11,1);
+		_uniformRoulette.sector(12,1);
+		_uniformRoulette.sector(13,1);
+		_uniformRoulette.sector(14,1);
+		_uniformRoulette.sector(15,1);
+		_uniformRoulette.sector(16,1);
+		_uniformRoulette.sector(17,1);
+		_uniformRoulette.sector(18,1);
+		_uniformRoulette.sector(19,1);
 
 	}
 	void tearDown() {
-		delete _uniformRoulette;
 	}
 protected:
 	void testSpinUniformWithMinimumForce() {
-		CPPUNIT_ASSERT_EQUAL(uint32(10),_uniformRoulette->spin(0));
+		CPPUNIT_ASSERT_EQUAL(uint32(10),_uniformRoulette.spin(0));
 	}
 	void testSpinUniformWithMaximumForce() {
-		CPPUNIT_ASSERT_EQUAL(uint32(19),_uniformRoulette->spin(9));
+		CPPUNIT_ASSERT_EQUAL(uint32(19),_uniformRoulette.spin(9));
 	}
 	void testSpinUniformWithMediumForce() {
-		CPPUNIT_ASSERT_EQUAL(uint32(15),_uniformRoulette->spin(5));
+		CPPUNIT_ASSERT_EQUAL(uint32(15),_uniformRoulette.spin(5));
 	}
 	void testSpinUniformWithExtraForce() {
 		try {
-			_uniformRoulette->spin(10);
+			_uniformRoulette.spin(10);
 			CPPUNIT_FAIL("Extra spinned roulette, did not throw an exception");
 		}
 		catch (Bioscena::ErrAssertionFailed & e) {
@@ -125,27 +123,26 @@ protected:
 class RouletteDistributionTest : public CppUnit::TestCase {
 	typedef CppUnit::TestCase super;
 	CPPUNIT_TEST_SUITE( RouletteDistributionTest );
-	CPPUNIT_TEST( testUniformSectors );
+	CPPUNIT_TEST( testUniformDistribution );
+	CPPUNIT_TEST( testDescompensatedDistribution );
 	CPPUNIT_TEST_SUITE_END();
-	Roulette * _uniformRoulette;
+	Roulette _uniformRoulette;
 public:
 	void setUp()
 	{
-		_uniformRoulette = new Roulette;
-		_uniformRoulette->sector(10,1);
-		_uniformRoulette->sector(11,1);
-		_uniformRoulette->sector(12,1);
-		_uniformRoulette->sector(13,1);
-		_uniformRoulette->sector(14,1);
-		_uniformRoulette->sector(15,1);
-		_uniformRoulette->sector(16,1);
-		_uniformRoulette->sector(17,1);
-		_uniformRoulette->sector(18,1);
-		_uniformRoulette->sector(19,1);
+		_uniformRoulette.sector(10,1);
+		_uniformRoulette.sector(11,1);
+		_uniformRoulette.sector(12,1);
+		_uniformRoulette.sector(13,1);
+		_uniformRoulette.sector(14,1);
+		_uniformRoulette.sector(15,1);
+		_uniformRoulette.sector(16,1);
+		_uniformRoulette.sector(17,1);
+		_uniformRoulette.sector(18,1);
+		_uniformRoulette.sector(19,1);
 
 	}
 	void tearDown() {
-		delete _uniformRoulette;
 	}
 protected:
 	void assertDistributionErrorAcceptable(
@@ -153,7 +150,7 @@ protected:
 			uint32 niterations,
 			uint32 nbins) {
 		std::stringstream log("Histogram mismatch:\n");
-		uint32 tolerance=niterations/100;
+		uint32 tolerance=niterations/10; // A magnitude order
 		bool allMatch = true;
 		for (uint32 i = 10; i--;) {
 			uint32 difference = 
@@ -167,20 +164,38 @@ protected:
 		CPPUNIT_ASSERT_MESSAGE(log.str().c_str(),allMatch);
 	}
 protected:
-	void testUniformSectors() {
-		const uint32 niterations = 0xfffff;
+	void testUniformDistribution() {
+		const uint32 niterations = 0xffff;
 		const uint32 nbins = 10;
 		const uint32 offset = 10;
 		uint32 histograma[nbins] = {};
 //		time_t t= time(NULL);
 		for (int i = niterations; i--;) {
-			uint32 value=(*_uniformRoulette);
+			uint32 value=_uniformRoulette;
 			CPPUNIT_ASSERT(value>=offset && value <offset+nbins);
 			histograma[value-offset]++;
 		}
 
 //		std::cout << "Elapsed preallocated object: " << (time(NULL)-t) << std::endl;
 		assertDistributionErrorAcceptable(histograma,niterations,nbins);
+
+	}
+	void testDescompensatedDistribution() {
+		Roulette descompensatedRoulette;
+		descompensatedRoulette.sector(1,1);
+		// This one is 3 times bigger
+		descompensatedRoulette.sector(2,3);
+
+		const uint32 niterations = 0xffff;
+		uint32 matchesOne = 0;
+		uint32 matchesTwo = 0;
+		for (int i = niterations; i--;) {
+			uint32 value=descompensatedRoulette;
+			if (value==1) matchesOne++;
+			else if (value==2) matchesTwo++;
+			else CPPUNIT_FAIL("Unexpected value");
+		}
+		CPPUNIT_ASSERT_DOUBLES_EQUAL(matchesOne*3,matchesTwo,niterations/100.0);
 
 	}
 };
