1d1fff2bfcc6bf85acce17ebe74374f3ce92d9e5
David García Garzón <voki@canvoki.net>
2004-07-31 21:19:07 +0000

    Clean up

--- src/Utils/BioIncludes.h
+++ src/Utils/BioIncludes.h
@@ -1,24 +1,8 @@
 #ifndef __BIOINCLUDES_H__
 #define __BIOINCLUDES_H__
 
-#include <iostream>
-#include <assert.h>
-//#include "Missatger.h"
 #include "Portable.hxx"
 #include "Assert.hxx"
-#if 0
-// Definim el nostre assert
-//#if (defined(_MSVC)&&defined(_DEBUG))||(!defined(_MSVC)&&!defined(NDEBUG)
-#define KKEPAssert(expr, msg) ((expr)||((error<<__FILE__<<":"<<__LINE__<<":"<<msg<<endl) ,true))
-//#else
-//#define KKEPAssert(expr, msg)
-//#endif // 
-#endif
-
-// Para quitar el warning de que hay nombres de simbolos demasiado grandes
-#ifdef  _MSC_VER
-	#pragma warning(disable:4786)
-#endif /* _MSC_VER */
 
 #endif //__BIOINCLUDES_H__
 
