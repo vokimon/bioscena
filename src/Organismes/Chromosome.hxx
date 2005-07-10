aef559908b8cee8ceebff7cb7bc75e6c2d059352
David García Garzón <voki@canvoki.net>
2005-07-10 22:41:15 +0000

    Operators added

--- src/Organismes/Chromosome.hxx
+++ src/Organismes/Chromosome.hxx
@@ -1,11 +1,10 @@
 
 #include "BioIncludes.h"
 #include <vector>
+#include <iomanip>
 
 // TODO:
 // Random init
-// Sequence init (to do testing)
-// fusion with another cromosome
 // partition by a centromer
 // remove codons
 // add codons
@@ -19,6 +18,7 @@ namespace Bioscena
 		// Inner Types
 		public:
 			typedef uint32 Codon;
+			typedef std::vector<Codon> Codons;
 		// Construction/Destruction
 		public:
 			Cromosome() {}
@@ -26,11 +26,58 @@ namespace Bioscena
 			{
 				KKEP_ASSERT(position<size(),
 					"Accessing to a non-existent codon.");
-				return _codons[position];
+				return _codons.at(position);
+			}
+			void initSequence(uint length, uint start=0)
+			{
+				_codons.resize(length);
+				for (uint32 i = 0; i<length; i++)
+					_codons[i]=start+i;
 			}
 			uint32 size() const {return _codons.size();}
+
+			std::string asString()
+			{
+				std::ostringstream os;
+				dumpOn(os);
+				return os.str();
+			}
+			void dumpOn(std::ostream & os)
+			{
+				os << '[';
+				os << std::setfill('0');
+				os << std::hex;
+				for (uint32 i = 0; i<size(); i++)
+				{
+					if (i) os << ':';
+					os 
+						<< std::setw(8)
+						<< _codons[i];
+				}
+				os << std::dec;
+				os << ']';
+			}
+			void fuse(const Cromosome & toFuse)
+			{
+				const Codons & newCodons = toFuse._codons;
+				_codons.insert(_codons.end(),
+					newCodons.begin(),
+					newCodons.end());
+			}
+			bool split(unsigned int centromer, Cromosome & splitted)
+			{
+				if (centromer>=_codons.size()) return false;
+				if (centromer==0) return false;
+				splitted._codons.assign(
+					_codons.begin()+centromer,
+					_codons.end());
+				_codons.erase(
+					_codons.begin()+centromer,
+					_codons.end());
+				return true;
+			}
 		private:
-			std::vector<Codon> _codons;
+			Codons _codons;
 	};
 
 }
