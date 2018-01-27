e49e138b7ff5b6a6df8e207d345a0f8edb63617c
David García Garzón <voki@canvoki.net>
2018-01-27 19:25:24 +0100

    updated build

--- src/build.h
+++ src/build.h
@@ -1,7 +1,7 @@
 // Build Number Generator v1.4
 // Copyright (c) 1998-2001 Vokimon/KKEPerians UNLTD
-#define BuildNumber 0x1016
-#define BuildDate "20040729 02:38:25"
+#define BuildNumber 0x1045
+#define BuildDate "20180119 20:21:03"
 // Don't modify anything in this file but the hexadecimal number above
 // If something gets wrong, delete this file, run the generator, and
 // modify the hex number acordantly with the last version you remember
